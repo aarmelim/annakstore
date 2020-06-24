1a5741309ef3c637155b3b55ba486ce57cc4ccd3
Adriano Mantovani Armelim <aarmelim@hotmail.com>
2020-06-24 19:13:16 -0300

    fazendo em SPA e limpando código

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-107251954-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments)
+  };
+  gtag('js', new Date());
+  gtag('config', 'UA-107251954-1');
+</script>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Anna K Store - Loja de roupas femininas">
+  <meta name="author" content="Anna K Store">
+
+  <title>Anna K. Store</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/one-page-wonder.css" rel="stylesheet">
+
+  <link href="css/personalizado.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Barra de navegacao -->
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">Anna K. Store</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="default.htm">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="produtos.php">Produtos</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+  <header>
+    <div class="overlay">
+      <div class="container">
+        <img src="img/fundo.jpg" width="100%" class="img-responsive" alt="Imagem Anna K Store - roupas femininas">
+      </div>
+    </div>
+  </header>
+  <hr />
+
+  <div class="container">
+
+    <div class="row">
+      <div class="col-lg-4">
+
+        <h4>Moda</h4>
+        <p>Oferecemos trabalho de Consultoria de Moda e Imagem focando nas necessidades pessoais e formato visual. Criamos looks que se encaixam e atendam todas as necessidades e desejos das pessoas.</p>
+
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+
+        <h4>Estilo</h4>
+        <p>Aqui na Anna K você encontra roupas lindas, cheias de estilo e qualidade e dicas de styling e beleza.</p>
+
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+
+        <h4>Comodidade</h4>
+        <p>Roupas para mulheres de diferentes estilos. Compre sem sair de casa. Enviamos nossas pe?as para todo o Brasil e aceitamos Cart?o de Débito e Crédito. Acesse nossas novidades através das redes sociais.</p>
+
+      </div><!-- /.col-lg-4 -->
+    </div><!-- /.row -->
+  </div>
+
+  <hr />
+
+  <div class="container">
+
+    <div class="row">
+      <div class="col-lg-8 col-lg-offset-2">
+        <h3>Entre em contato</h3>
+        <br>
+      </div>
+      <div class="col-lg-8 col-lg-offset-2">
+        <form enctype="text/plain" method="post" name="formContato" class>
+            <div class="row">
+              <div class="col-md-12">
+                <div class="form-group">
+                  <label for="form_email">Email</label>
+                  <input id="email" type="email" name="email" class="form-control" placeholder="email">
+                </div>
+              </div>
+            </div>  
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="form_name">Nome Completo (opcional)</label>
+                  <input id="nome" type="text" name="nome" class="form-control" placeholder="Nome completo">
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-9">
+                <div class="form-group">
+                  <label for="form_phone">Telefone (opcional)</label>
+                  <input id="telefone" type="tel" name="telefone" class="form-control" placeholder="Telefone (Pode ser seu celular)">
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-9">
+                <div class="form-group">
+                  <label for="form_message">O que deseja saber (opcional)</label>
+                  <textarea id="mensagem" name="mensagem" class="form-control" placeholder="Sua mensagem" rows="4"></textarea>
+                </div>
+              </div>
+              <div class="col-md-12">
+                <input type="submit" class="btn btn-success btn-send" value="Enviar mensagem">
+              </div>
+            </div>
+        </form>
+      </div>
+    </div>
+
+  </div>
+  <hr />
+    <div class="container">
+</div>
+  <!-- Footer -->
+  <footer class="py-3 bg-dark">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-3 col-md-3" id="icone-wa">
+          <img class="img-fluid rounded" src="img/wa_icone.png" width=25em alt="telefone whats app 11-98402.2526"> <a class="m-0 text-center text-white" href="https://api.whatsapp.com/send?phone=5511981207355&text=Ola">11-98402.2526</a>
+        </div>
+
+        <div class="col-xs-12 col-sm-3 col-md-3 " id="icone-rede-social">
+          <a href="http://www.instagram.com/anna_k_store"><img class="img-fluid rounded" src="img/instagram.ico" width=30em alt="instragram @anna_k_store"></a>
+          <a href="http://www.facebook.com/annakstore"><img class="img-fluid rounded" src="img/fb.ico" width=25em alt="facebook.com/annakstore"></a>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-6">
+          <a target="_blank" class="text-white" href="mailto:storeannak@gmail.com">storeannak@gmail.com</a>
+        </div>
+
+      </div>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="js/jquery/jquery.min.js"></script>
+  <script src="js/popper/popper.min.js"></script>
+  <script src="js/bootstrap/bootstrap.min.js"></script>
+
+</body>
+
+</html>
